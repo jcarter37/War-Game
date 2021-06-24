4ea68a9e8d8fa4aefa4f19ba771630bb9baaa966
Jordan <jordancarter@Jordans-MacBook-Pro.local>
2021-06-24 11:53:41 -0700

    minor updates

--- js/main.js
+++ js/main.js
@@ -6,15 +6,19 @@ const faceValues = {J: 11, Q: 12, K: 13, A: 14};
 const masterDeck = createDeck()
 
 
-
 /*----- app's state (variables) -----*/
 let deck, playerDeck, computerDeck, playerCard, computerCard, pDeckCount, cDeckCount, playerWarCards, computerWarCards, winner
 
 
 
 /*----- cached element references -----*/
-const pDeckEl = document.querySelector('#player-stack');
-const cDeckEl = document.querySelector('#comp-stack');
+const pDeckEl = document.querySelector('#player-card');
+const cDeckEl = document.querySelector('#comp-card');
+const pStackEl = document.querySelector("player-stack");
+const cStackEl = document.querySelector("computer-stack");
+
+
+
 
 const pScoreEl = document.querySelector('#player-score');
 const cScoreEl = document.querySelector('#comp-score');
@@ -23,12 +27,12 @@ const cScoreEl = document.querySelector('#comp-score');
 
 /*----- event listeners -----*/
 document.querySelector('#play').addEventListener('click', drawCard);
-
+document.querySelector('#reset').addEventListener('click', initialize);
 
 
 /*----- functions -----*/
 function initialize() {
-    deck = shuffleDeck(masterDeck);
+    deck = shuffleDeck([...masterDeck]);
     playerDeck = deck.splice(0, 26);
     computerDeck = deck
     playerWarCards = [];
@@ -76,6 +80,7 @@ function drawCard(){
 
 
 function compareCards(){
+    checkWinner();
     if(playerCard.Rank > computerCard.Rank) {
         playerDeck.push(playerCard);
         playerDeck.push(computerCard);
@@ -96,10 +101,12 @@ function render() {
     if ("Suit" in playerCard && "Suit" in computerCard) {
         pDeckEl.innerHTML = `<div class="card ${playerCard.Suit}"></div>`;
 	    cDeckEl.innerHTML = `<div class="card ${computerCard.Suit}"></div>`;
-    }   
+        pStackEl.innerHTML = `<div class="card ${playerWarCards.Suit}"></div>`;
+        cStackEl.innerHTML = `<div class="card ${computerWarCards.Suit}"></div>`;
+    }
 }
-
 function handleWar() {
+    checkWinner();
     // compare the first card in each to see who wins
     playerWarCards = [...playerDeck.splice(0, 4), ...playerWarCards];
     computerWarCards = [...computerDeck.splice(0, 4), ...computerWarCards];
@@ -124,7 +131,15 @@ function handleWar() {
 }
 
 
-
+function checkWinner() {
+    if (playerDeck.length === 0){
+        winner = true;
+        console.log("Computer Wins")
+    } else if (computerDeck.length === 0) {
+        console.log("Player Wins")
+        winner = true;
+    } 
+}
 // if playerdeck 0 || computer  deck 0 
 // call check winner at beginning of handle war
 
