d712365b6d11ba3a15ab59872f8f0bb7b130a0bf
Jordan <jordancarter@Jordans-MacBook-Pro.local>
2021-06-24 20:37:24 -0700

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # War-Game
+
+Game Title: War
+
+    War is a two versus two card game. The deck of cards is shuffled and divided evenly between player and computer. The 
+    26 cards are set facedown and each player draws one card from the top of their deck to compare. The highest value card receives both cards to put at the base of their deck. If the cards drawn are of equal value the two players must then draw 4 more cards comparing the 4th card drawn. During war if the last card revealed is of equal value again repeat the steps until one player draws a higher value card winning the war. The winner of the war collects all to put at the base of their deck. A player wins by accumulating all 52 cards.
+
+Technologies Used:
+     HTML, CSS, JavaScript
+
+
+Screenshots:
+
+    ![Screen Shot 2021-06-24 at 7 39 48 PM](https://user-images.githubusercontent.com/85077994/123365276-d1322800-d52a-11eb-9210-7f11555d3e30.png)
+
+
+
+    ![Screen Shot 2021-06-24 at 8 25 16 PM](https://user-images.githubusercontent.com/85077994/123365390-0b032e80-d52b-11eb-8989-ee48de293fc1.png)
+
+
+
+    ![Screen Shot 2021-06-24 at 7 38 55 PM](https://user-images.githubusercontent.com/85077994/123365429-1e15fe80-d52b-11eb-86cc-fbfd733358c0.png)
+
+Getting Started: 
+
+
+Next Steps: Add set timeout feature and animations so the cards drawn during the flips and war don't seem like they are appearing from no where. Fix how the war card flips render on the screen when multiple wars happen.
--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-/*----- constants -----*/
-
-const suits = ["h", "d", "c", "s"]
-const ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"];
+const suits = ['h', 'd', 'c', 's']
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 const faceValues = {J: 11, Q: 12, K: 13, A: 14};
 const masterDeck = createDeck()
 
 
-/*----- app's state (variables) -----*/
+
 let deck, playerDeck, computerDeck, playerCard, computerCard, pDeckCount, cDeckCount, playerWarCards, computerWarCards, winner
 
 
 
-/*----- cached element references -----*/
+
 const pDeckEl = document.querySelector('#player-card');
 const cDeckEl = document.querySelector('#comp-card');
-const pStackEl = document.querySelector("#player-stack");
-const cStackEl = document.querySelector("#comp-stack");
+const pStackEl = document.querySelector('#player-stack');
+const cStackEl = document.querySelector('#comp-stack');
 const pWarEl = document.querySelector('#player-war');
 const cWarEl = document.querySelector('#comp-war');
 const pScoreEl = document.querySelector('#player-score');
@@ -23,12 +21,12 @@ const cScoreEl = document.querySelector('#comp-score');
 const pWinEl = document.querySelector('#p-win');
 const cWinEl = document.querySelector('#c-win');
 
-/*----- event listeners -----*/
+
 document.querySelector('#play').addEventListener('click', drawCard);
 document.querySelector('#reset').addEventListener('click', initialize);
 
 
-/*----- functions -----*/
+
 function initialize() {
     deck = shuffleDeck([...masterDeck]);
     playerDeck = deck.splice(0, 26);
@@ -54,8 +52,7 @@ function createDeck() {
             };
 			deck.push(card);
         })
-    })
-    console.log(deck);
+    });
     return deck;
 } 
 
@@ -72,25 +69,21 @@ function shuffleDeck(deck) {
 function drawCard(){
     playerCard = playerDeck.shift();
     computerCard = computerDeck.shift(); 
-    console.log(playerCard)
-    console.log(computerCard)
     compareCards();
 }
 
 
 function compareCards(){
-    pWarEl.innerHTML = "";
-    cWarEl.innerHTML = "";
+    pWarEl.innerHTML = '';
+    cWarEl.innerHTML = '';
     checkWinner();
     if(playerCard.Rank > computerCard.Rank) {
         playerDeck.push(playerCard);
         playerDeck.push(computerCard);
-        console.log(`Player wins with ${playerCard.Rank} of ${playerCard.Suit}`)
-    }else if(playerCard.Rank < computerCard.Rank) {
+    } else if(playerCard.Rank < computerCard.Rank) {
         computerDeck.push(computerCard);
         computerDeck.push(playerCard);
-        console.log(`Computer wins with ${computerCard.Rank} of ${computerCard.Suit}`)
-    } else {
+    } else{
         handleWar();
     }
     render();
@@ -100,40 +93,40 @@ function render() {
     if (playerDeck.length > 0) {
         pStackEl.innerHTML = `<div class="card back"></div>`;
     } else {
-        pStackEl.innerHTML = "";
+        pStackEl.innerHTML = '';
     } 
     if (computerDeck.length > 0) {
         cStackEl.innerHTML = `<div class="card back"></div>`;
     } else {
-        cStackEl.innerHTML = "";
+        cStackEl.innerHTML = '';
     } 
     if (playerDeck.length === 0){
-        cWinEl.innerHTML = "Computer Wins"
+        cWinEl.innerHTML = 'Computer Wins!'
     } else {
-        cWinEl.innerHTML = ""
+        cWinEl.innerHTML = '';
     } 
     if (computerDeck.length === 0) {
-        pWinEl.innerHTML = "Player Wins"
+        pWinEl.innerHTML = 'Player Wins!'
     } else {
-        pWinEl.innerHTML = "";
+        pWinEl.innerHTML = '';
     }
 
     pScoreEl.textContent = `Player Deck Count: ${playerDeck.length}`;
     cScoreEl.textContent = `Computer Deck Count: ${computerDeck.length}`;
-    pDeckEl.innerHTML = `<div class="card ${playerCard && playerCard.Suit}"></div>`;
-	cDeckEl.innerHTML = `<div class="card ${computerCard && computerCard.Suit}"></div>`;
+    pDeckEl.innerHTML = `<div class='card ${playerCard && playerCard.Suit}'></div>`;
+	cDeckEl.innerHTML = `<div class='card ${computerCard && computerCard.Suit}'></div>`;
     }
 
     
 function renderWarCards() {
-    let pTemplate = "";
+    let pTemplate = '';
     playerWarCards.forEach(function(card, idx) {
-        pTemplate += `<div class="card ${card.Suit} ${idx === 0 ? "shadow": ""}"></div>`
+        pTemplate += `<div class='card ${card.Suit} ${idx === 0 ? 'shadow': ''}'></div>`
     })
     pWarEl.innerHTML = pTemplate;
-    let cTemplate = "";
+    let cTemplate = '';
     computerWarCards.forEach(function (card, idx) {
-      cTemplate += `<div class="card ${card.Suit} ${idx === 0 ? "shadow": ""}"></div>`;
+      cTemplate += `<div class='card ${card.Suit} ${idx === 0 ? 'shadow': ''}'></div>`;
     });
     cWarEl.innerHTML = cTemplate;
 }
@@ -142,11 +135,9 @@ function renderWarCards() {
 
 function handleWar() {
     checkWinner();
-    // compare the first card in each to see who wins
     playerWarCards = [...playerDeck.splice(0, 4), ...playerWarCards];
     computerWarCards = [...computerDeck.splice(0, 4), ...computerWarCards];
     renderWarCards();
-    // grab 4 cards from each deck & keep any old cards   ... spread operator
     if (playerWarCards[0].Rank > computerWarCards[0].Rank) {
         playerDeck.push(...playerWarCards);
         playerDeck.push(...computerWarCards);
@@ -170,9 +161,7 @@ function handleWar() {
 function checkWinner() {
     if (playerDeck.length === 0){
         winner = true;
-        console.log("Computer Wins")
-    } else if (computerDeck.length === 0) {
-        console.log("Player Wins")
+    }else if (computerDeck.length === 0) {
         winner = true;
     } 
 }
